ebf6d1725951e4e31587d3501d56e65edcefb556
juliet owah <julietowah@gmail.com>
2023-11-15 14:04:42 +0100

    email system

--- meetup/src/pages/Form.jsx
+++ meetup/src/pages/Form.jsx
@@ -123,7 +123,7 @@ const Form = () => {
         <div>
             <label For="phone" className="block mb-2 text-xl font-medium text-gray-900 dark:text-gray-300">Phone number</label>
             <input value={phoneNumber}
-  onChange={handleChange} name="phoneNumber" type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="08078554668" required />
+  onChange={handleChange} name="phoneNumber" type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="08078554668 " pattern="[0-9]{11}" required />
         </div>
         <div>
             <label For="email" className="block mb-2 text-xl font-medium text-gray-900 dark:text-gray-300">Email</label>
